13d2f267635aa94b55e6f4de64c579b708afa935
Synthev <105394020+Synthird@users.noreply.github.com>
2024-09-15 15:35:45 -0700

    Update App.java

--- src/App.java
+++ src/App.java
@@ -8,6 +8,7 @@ public class App {
         System.out.print("Please enter a number you want to find the absolute value of: ");
         float value = scanner.nextFloat();
         float absoluteValue = findAbsoluteValue.findValue(value);
+        System.out.println("|" + value + "| = " + absoluteValue);
         System.out.println("The absolute value of " + value + " is " + absoluteValue);
     }
 }
