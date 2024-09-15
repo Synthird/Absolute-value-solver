638c58ec801200d8aa11e3408fb6f29d3d1ed8d7
Synthev <105394020+Synthird@users.noreply.github.com>
2024-09-15 15:25:45 -0700

    Add files via upload

---
+++ src/App.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class App {
+    static Scanner scanner = new Scanner(System.in);
+    static FindAbsoluteValue findAbsoluteValue = new FindAbsoluteValue();
+
+    public static void main(String[] args) throws Exception {
+        System.out.print("Please enter a number you want to find the absolute value of: ");
+        float value = scanner.nextFloat();
+        float absoluteValue = findAbsoluteValue.findValue(value);
+        System.out.println("The absolute value of " + value + " is " + absoluteValue);
+    }
+}
---
+++ src/FindAbsoluteValue.java
@@ -0,0 +1,5 @@
+public class FindAbsoluteValue {
+    public float findValue(float number) {
+        return number < 0 ? number * -1 : number;
+    }
+}
