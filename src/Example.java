6b7f1c6808c8e3aaee86350d25d3e141b455570b
Synthird <105394020+Synthird@users.noreply.github.com>
2024-09-28 17:52:28 -0700

    Update and rename App.java to Example.java

--- src/Example.java
+++ src/Example.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class App {
+public class Example {
     static Scanner scanner = new Scanner(System.in);
     static FindAbsoluteValue findAbsoluteValue = new FindAbsoluteValue();
 
